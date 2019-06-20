6a7566efe795aa7e9645968ccbac8d67b0b4f7bb
MarvinJimenez13 <marvinjiga@gmail.com>
2019-06-19 22:10:36 -0500

    Lección 25.

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -5,26 +5,7 @@
 @section('content')
 
   {!! Form::open(['route' => 'trainers.store', 'method' => 'POST', 'files' => true]) !!}
-  <div class="form-group">
-    {!! Form::label('name', 'Nombre') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-  </div>
-
-  <div class="form-group">
-    {!! Form::label('avatar', 'Avatar') !!}
-    {!! Form::file('avatar') !!}
-  </div>
-
-  <div class="form-group">
-    {!! Form::label('slug', 'Slug') !!}
-    {!! Form::text('slug', null, ['class' => 'form-control']) !!}
-  </div>
-
-  <div class="form-group">
-    {!! Form::label('description', 'Descripción') !!}
-    {!! Form::text('description', null, ['class' => 'form-control']) !!}
-  </div>
-
+  @include('trainers.form')
   {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
   {!! Form::close() !!}
 
